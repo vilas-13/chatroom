edd011b4131faf58789a8be5fdb3f775c8b246b4
vscharan <charanv369@gmail.com>
2025-02-13 16:41:00 +0530

    updated ChatroomApplication.java for war to run in tomcat

--- src/main/java/com/goldencat/chatroom/ChatroomApplication.java
+++ src/main/java/com/goldencat/chatroom/ChatroomApplication.java
@@ -2,14 +2,15 @@ package com.goldencat.chatroom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
-public class ChatroomApplication {
+public class ChatroomApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChatroomApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ChatroomApplication.class, args);
+    }
 }
